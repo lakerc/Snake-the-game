77005f8b284993602a3eb184cc17b38ede982dcd
AlexTerra21 <baewoo21@gmail.com>
2017-08-12 05:37:40 +0300

    Добавлены ранее не добавленные файлы

---
+++ Cell.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake
+{
+    class Cell
+    {
+        public string Val { get; set; }
+        public ConsoleColor Color { get; set; }
+
+        public Cell(string aVal, ConsoleColor aColor)
+        {
+            Val = aVal;
+            Color = aColor;
+        }
+    }
+}
