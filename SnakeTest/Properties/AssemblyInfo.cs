8631ee2f54bc5dfb9fbea971ba8830371f358468
Sam SSD <burnss9@nku.edu>
2017-09-22 12:56:22 -0400

    Added test class for Snake

---
+++ SnakeTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SnakeTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SnakeTest")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("1644d1f6-781e-43ab-b05d-9c50beb7c2a0")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ SnakeTest/SnakeTest.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using SnakeGame;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SnakeGameTest
+{
+    [TestClass]
+    public class SnakeTest
+    {
+        [TestMethod]
+        public void SnakeMoveUpTest()
+        {
+            Snake s = new Snake(0, 0);
+            s.DirectionUp();
+            s.Move();
+
+            Assert.AreEqual(-1, s.Head.Y);
+        }
+
+        [TestMethod]
+        public void SnakeMoveDownTest()
+        {
+            Snake s = new Snake(0, 0);
+            s.DirectionDown();
+            s.Move();
+
+            Assert.AreEqual(1, s.Head.Y);
+        }
+
+        [TestMethod]
+        public void SnakeMoveRightTest()
+        {
+            Snake s = new Snake(0, 0);
+            s.DirectionRight();
+            s.Move();
+
+            Assert.AreEqual(1, s.Head.X);
+        }
+
+        [TestMethod]
+        public void SnakeMoveLeftTest()
+        {
+            Snake s = new Snake(0, 0);
+            s.DirectionLeft();
+            s.Move();
+
+            Assert.AreEqual(-1, s.Head.Y);
+        }
+
+
+    }
+}
