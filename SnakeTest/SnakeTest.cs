abe954f9f1b76896265f95408eb3bf2e158968ba
Sam SSD <burnss9@nku.edu>
2017-09-22 18:39:58 -0400

    Fixed tests and made them cover more

--- SnakeTest/SnakeTest.cs
+++ SnakeTest/SnakeTest.cs
@@ -10,41 +10,45 @@ namespace SnakeGameTest
         [Test]
         public void SnakeMoveUpTest()
         {
-            Snake s = new Snake(new Point(0, 0), new GameField(20, 20));
+            Snake s = new Snake(new Point(10, 10), new GameField(20, 20));
             s.DirectionUp();
             s.Move();
 
-            Assert.AreEqual(-1, s.Head.Y);
+            Assert.AreEqual(9, s.Head.Y);
+            Assert.AreEqual(10, s.Head.X);
         }
 
         [Test]
         public void SnakeMoveDownTest()
         {
-            Snake s = new Snake(new Point(0, 0), new GameField(20, 20));
+            Snake s = new Snake(new Point(10, 10), new GameField(20, 20));
             s.DirectionDown();
             s.Move();
 
-            Assert.AreEqual(1, s.Head.Y);
+            Assert.AreEqual(11, s.Head.Y);
+            Assert.AreEqual(10, s.Head.X);
         }
 
         [Test]
         public void SnakeMoveRightTest()
         {
-            Snake s = new Snake(new Point(0, 0), new GameField(20, 20));
+            Snake s = new Snake(new Point(10, 10), new GameField(20, 20));
             s.DirectionRight();
             s.Move();
 
-            Assert.AreEqual(1, s.Head.X);
+            Assert.AreEqual(11, s.Head.X);
+            Assert.AreEqual(10, s.Head.Y);
         }
 
         [Test]
         public void SnakeMoveLeftTest()
         {
-            Snake s = new Snake(new Point(0, 0), new GameField(20, 20));
+            Snake s = new Snake(new Point(10, 10), new GameField(20, 20));
             s.DirectionLeft();
             s.Move();
 
-            Assert.AreEqual(-1, s.Head.X);
+            Assert.AreEqual(9, s.Head.X);
+            Assert.AreEqual(10, s.Head.Y);
         }
 
         [Test]
